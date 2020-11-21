9241bdc69b902c2888491a7d3a265d9188a9d6c2
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2020-11-21 09:14:55 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ plugin_author = "Louis Sarwal, formerly Paul Goddard"
 plugin_author_email = "louis.sarwal@gmail.com"
 
 # The plugin's homepage URL. Can be overwritten within OctoPrint's internal data via __plugin_url__ in the plugin module
-plugin_url = "https://github.com/nukeem/continuousprint"
+plugin_url = "https://github.com/Zinc-OS/continuousprint"
 
 # The plugin's license. Can be overwritten within OctoPrint's internal data via __plugin_license__ in the plugin module
 plugin_license = "AGPLv3"
