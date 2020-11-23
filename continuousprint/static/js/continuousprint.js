d82b767930c980fe9bb3d01a7a1568f0158f9d84
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2020-11-23 16:30:07 -0500

    Update continuousprint.js

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -42,7 +42,7 @@ $(function() {
 								var other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+i+"'></i>&nbsp; <i style='cursor: pointer' class='fa fa-chevron-up' data-index='"+i+"'></i>&nbsp;";
 								if (i == 0) other = "";
 								if (i == 1) other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+i+"'></i>&nbsp;";
-								row = $("<div style='padding: 10px;border-bottom: 1px solid #000;"+(i==0 ? "background: #f9f4c0;" : "")+"'>"+file.name+"<div class='pull-right'>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+i+"'></i></div></div>");
+								row = $("<div style='padding: 10px;border-bottom: 1px solid #000;"+(i==0 ? "background: #f9f4c0;" : "")+"'>"+file.name+"<input type = 'text' id='print"+i.toString()+"' placeholder='"+file.count.toString()+"'/><div class='pull-right'>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+i+"'></i></div></div>");
 								row.find(".fa-minus").click(function() {
 									self.removeFromQueue($(this).data("index"));
 								});
@@ -96,7 +96,8 @@ $(function() {
 								self.addToQueue({
 									name: $(this).data("name"),
 									path: $(this).data("path"),
-									sd: $(this).data("sd")
+									sd: $(this).data("sd"),
+                                    count: 1
 								});
 							});
 							$('#file_list').append(row);
