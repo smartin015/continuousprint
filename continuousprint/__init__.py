2c46c4e4625e97b5d98fb17ad399a1eb93f8f4e9
Patrick <patricklo@gmail.com>
2020-12-19 00:17:41 -0800

    Fix bug: Count should be an integer

--- continuousprint/__init__.py
+++ continuousprint/__init__.py
@@ -212,7 +212,7 @@ class ContinuousprintPlugin(octoprint.plugin.SettingsPlugin,
 			name=flask.request.form["name"],
 			path=flask.request.form["path"],
 			sd=flask.request.form["sd"],
-			count=flask.request.form["count"]
+			count=int(flask.request.form["count"])
 		))
 		self._settings.set(["cp_queue"], json.dumps(queue))
 		self._settings.save()
