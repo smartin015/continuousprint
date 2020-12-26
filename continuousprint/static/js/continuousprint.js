54bdb59f1615fd4aa6e6de041b77d27c828cfc9a
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2020-12-26 11:12:31 -0500

    Update continuousprint.js

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -45,7 +45,7 @@ $(function() {
 								var other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+i+"'></i>&nbsp; <i style='cursor: pointer' class='fa fa-chevron-up' data-index='"+i+"'></i>&nbsp;";
 								if (i == 0) other = "";
 								if (i == 1) other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+i+"'></i>&nbsp;";
-								row = $("<div style='padding: 10px;border-bottom: 1px solid #000;"+(i==0 ? "background: #f9f4c0;" : "")+"'>" + "<input class='fa fa-text' type = 'text' style='width: 20px; height: 12px;' data-index='"+i+"' value='"+file.count.toString()+"'/><div style='max-width: 200px; overflow-x:scroll'> " + file.name + "</div><div class='pull-right'>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+i+"'></i></div></div>");
+								row = $("<div style='padding: 10px;border-bottom: 1px solid #000;"+(i==0 ? "background: #f9f4c0;" : "")+"'>" + "<input class='fa fa-text' type = 'text' style='width: 20px; height: 12px;' data-index='"+i+"' value='"+file.count.toString()+"'/>&nbsp<div style='max-width: 300px; overflow-x:scroll'> " + file.name + "</div><div class='pull-right'>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+i+"'></i></div></div>");
 								row.find(".fa-minus").click(function() {
 									self.removeFromQueue($(this).data("index"));
 								});
@@ -81,7 +81,7 @@ $(function() {
 									}
 								}
 								
-								row = $("<div style='padding: 15px; border-bottom: 1px solid #000;background:#c2fccf'>Complete: <div style='max-width: 200px; overflow-x:scroll'>" + file.name + "</div> <div class='pull-right'>average time: " + time.toFixed(0) + suffix + " Times run:" + file.times_run + "</div></div>")
+								row = $("<div style='padding: 15px; border-bottom: 1px solid #000;background:#c2fccf'>Complete: &nbsp<div style='max-width: 300px; overflow-x:scroll'>" + file.name + "</div> <div class='pull-right'>average time: " + time.toFixed(0) + suffix + " Times run:" + file.times_run + "</div></div>")
 							}
 							$('#queue_list').append(row);
 						}
