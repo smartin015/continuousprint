7c45a1bd7d82752c40b3493534878c4a8e84f774
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2020-12-28 09:59:37 -0500

    Move buttons to the right
    removing the buttons from the flex-container

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -48,7 +48,7 @@ $(function() {
 								var other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+i+"'></i>&nbsp; <i style='cursor: pointer' class='fa fa-chevron-up' data-index='"+i+"'></i>&nbsp;";
 								if (i == 0) other = "";
 								if (i == 1) other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+i+"'></i>&nbsp;";
-								row = $("<div style='display: flex; flex-direction: row; justify-content: space-between; align-items: stretch; padding: 10px;border-bottom: 1px solid #000;"+(i==0 ? "background: #f9f4c0;" : "")+"'>" +"<input class='fa fa-text' type = 'text' style=' flex-grow: "+self.textGrow[i]+";flex-shrink:6; max-width:120px; max-height:8px;' data-index='"+i+"' value='" + file.count.toString() + "'/><p style='flex-shrink: 10; overflow-x:scroll;'> " + file.name + "</p><div style='flex-grow: 3; flex-shrink: 0; align-self: flex-end;'>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+i+"'></i></div></div>");
+								row = $("<div style='padding: 10px;border-bottom: 1px solid #000;"+(i==0 ? "background: #f9f4c0;" : "")+"'><div style='display: flex; flex-direction: row; justify-content: space-between; align-items: stretch;'><input class='fa fa-text' type = 'text' style=' flex-grow: "+self.textGrow[i]+";flex-shrink:6; max-width:120px; max-height:8px;' data-index='"+i+"' value='" + file.count.toString() + "'/><p style='flex-shrink: 10; overflow-x:scroll;'> " + file.name + "</p></div><div class= 'pull-right'>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+i+"'></i></div></div>");
 								row.find(".fa-minus").click(function() {
 									self.removeFromQueue($(this).data("index"));
 								});
