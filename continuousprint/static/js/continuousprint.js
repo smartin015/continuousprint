af986fc97fbf3a5c9c63088991cbd246241dddf1
Louis Sarwal <63823426+Zinc-OS@users.noreply.github.com>
2020-12-31 18:21:53 -0500

    Clear Queue if queue is empty

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -100,7 +100,7 @@ $(function() {
                     var row;
                     var Enter = false;
                     var other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+self.itemsInQueue+"'></i>&nbsp; <i style='cursor: pointer' class='fa fa-chevron-up' data-index='"+self.itemsInQueue+"'></i>&nbsp;";
-                    if (self.itemsInQueue == 0) {other = "";}
+                    if (self.itemsInQueue == 0) {other = "";$('#queue_list').html("");}
                     if (self.itemsInQueue == 1) {other = "<i style='cursor: pointer' class='fa fa-chevron-down' data-index='"+self.itemsInQueue+"'></i>&nbsp;";}
                     row = $("<div style='padding: 10px;border-bottom: 1px solid #000;"+(self.itemsInQueue==0 ? "background: #f9f4c0;" : "")+"'><div class='queue-row-container'><div class='queue-inner-row-container'><input class='fa fa-text count-box' type = 'text' data-index='"+self.itemsInQueue+"' value='" + file.count.toString() + "'/><p class='file-name' > " + file.name + "</p></div><div>" + other + "<i style='cursor: pointer' class='fa fa-minus text-error' data-index='"+self.itemsInQueue+"'></i></div></div></div>");
                     row.find(".fa-minus").click(function() {
