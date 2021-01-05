5b41d632ae0629cba71c4584a064582a76addd91
Zinc-OS <63823426+Zinc-OS@users.noreply.github.com>
2021-01-05 14:46:18 -0500

    Update continuousprint.js

--- continuousprint/static/js/continuousprint.js
+++ continuousprint/static/js/continuousprint.js
@@ -110,7 +110,7 @@ $(function() {
                                     suffix = " days";
                                 }
                             }
-                            row = $("<div style='padding: 15px; border-bottom: 1px solid #000;background:#c2fccf'><div class='queue-row-container'><div class='queue-inner-row-container'><p class='file-name' > Complete:" + file.name +"Print Times:" + file.title + "</p></div>Times run:"+file.times_run+"Average Time:"+time.toFixed+suffix+"</div></div></div");
+                            row = $("<div style='padding: 15px; border-bottom: 1px solid #000;background:#c2fccf'><div class='queue-row-container'><div class='queue-inner-row-container'><p class='file-name' > Complete: " + file.name +" " + file.title + "</p></div>Times run:"+file.times_run+"Average Time:"+time.toFixed()+suffix+"</div></div></div");
                        
                             $('#print_history').append(row);
                         }
