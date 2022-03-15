28ed8bbf97e12dc4473f4633eab8eab482e7d22c
Scott Martin <smartin015@gmail.com>
2022-03-15 13:17:52 -0400

    Further adjust travis.yml for python test environment

--- setup.py
+++ setup.py
@@ -66,40 +66,32 @@ additional_setup_parameters = {}
 from setuptools import setup
 import os
 
-if os.getenv("TRAVIS_CI") != "1":
-    try:
-            import octoprint_setuptools
-    except:
-            print("Could not import OctoPrint's setuptools, are you sure you are running that under "
-                  "the same python installation that OctoPrint is installed under?")
-            import sys
-            sys.exit(-1)
+try:
+        import octoprint_setuptools
+except:
+        print("Could not import OctoPrint's setuptools, are you sure you are running that under "
+              "the same python installation that OctoPrint is installed under?")
+        import sys
+        sys.exit(-1)
 
-    setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
-            identifier=plugin_identifier,
-            package=plugin_package,
-            name=plugin_name,
-            version=plugin_version,
-            description=plugin_description,
-            author=plugin_author,
-            mail=plugin_author_email,
-            url=plugin_url,
-            license=plugin_license,
-            requires=plugin_requires,
-            additional_packages=plugin_additional_packages,
-            ignored_packages=plugin_ignored_packages,
-            additional_data=plugin_additional_data
-    )
+setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
+        identifier=plugin_identifier,
+        package=plugin_package,
+        name=plugin_name,
+        version=plugin_version,
+        description=plugin_description,
+        author=plugin_author,
+        mail=plugin_author_email,
+        url=plugin_url,
+        license=plugin_license,
+        requires=plugin_requires,
+        additional_packages=plugin_additional_packages,
+        ignored_packages=plugin_ignored_packages,
+        additional_data=plugin_additional_data
+)
 
-    if len(additional_setup_parameters):
-            from octoprint.util import dict_merge
-            setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
-else:
-    setup_parameters = dict(
-            packages=[plugin_package],
-            name=plugin_name,
-            version=plugin_version,
-            description=plugin_description,
-            )
+if len(additional_setup_parameters):
+        from octoprint.util import dict_merge
+        setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
 
 setup(**setup_parameters)
