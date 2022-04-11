63452a798e912bf2af38a093cbbaef29dbad3a55
Scott Martin <smartin015@gmail.com>
2022-04-11 12:34:38 -0400

    Add tests for printer settings, add descriptions to settings and add contributing section to scripting guide

--- continuousprint/static/js/continuousprint_settings.js
+++ continuousprint/static/js/continuousprint_settings.js
@@ -1,20 +1,29 @@
-function CPSettingsViewModel(parameters) {
+if (typeof log === "undefined" || log === null) {
+  // In the testing environment, dependencies must be manually imported
+  ko = require('knockout');
+  log = {
+    "getLogger": () => {return console;}
+  };
+  CP_PRINTER_PROFILES = [];
+  CP_GCODE_SCRIPTS = [];
+}
+
+function CPSettingsViewModel(parameters, profiles=CP_PRINTER_PROFILES, scripts=CP_GCODE_SCRIPTS) {
     var self = this;
     self.PLUGIN_ID = "octoprint.plugins.continuousprint";
     self.log = log.getLogger(self.PLUGIN_ID);
     self.settings = parameters[0];
 
     // Constants defined in continuousprint_settings.jinja2, passed from the plugin (see `get_template_vars()` in __init__.py)
-    try {
     self.profiles = {};
-    for (let prof of CP_PRINTER_PROFILES) {
+    for (let prof of profiles) {
       if (self.profiles[prof.make] === undefined) {
         self.profiles[prof.make] = {};
       }
       self.profiles[prof.make][prof.model] = prof;
     }
     self.scripts = {};
-    for (let s of CP_GCODE_SCRIPTS) {
+    for (let s of scripts) {
       self.scripts[s.name] = s.gcode;
     }
 
@@ -33,8 +42,8 @@ function CPSettingsViewModel(parameters) {
       return result;
     });
 
-    self.modelChanged = function(e) {
-      let profile = self.profiles[self.selected_make()][self.selected_model()];
+    self.modelChanged = function() {
+      let profile = (self.profiles[self.selected_make()] || {})[self.selected_model()];
       if (profile === undefined) {
         return;
       }
@@ -42,12 +51,13 @@ function CPSettingsViewModel(parameters) {
       cpset.cp_bed_clearing_script(self.scripts[profile.defaults.clearBed]);
       cpset.cp_queue_finished_script(self.scripts[profile.defaults.finished]);
     }
-    } catch(e) {
-    console.error(e);
-    }
 }
 
 
 try {
-module.exports = CPSettingsViewModel;
+module.exports = {
+  CPSettingsViewModel,
+  CP_PRINTER_PROFILES,
+  CP_GCODE_SCRIPTS,
+};
 } catch {}
