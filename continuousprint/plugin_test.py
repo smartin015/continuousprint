060504211eefe55eb085eb5f2855d90b836c7b5a
Scott Martin <smartin015@gmail.com>
2022-11-17 09:44:23 -0500

    Add more safety checks on LAN queue and Fileshare initialization, prevent network failures from taking down the plugin

--- continuousprint/plugin_test.py
+++ continuousprint/plugin_test.py
@@ -27,8 +27,14 @@ class MockSettings:
     def get(self, k):
         return self.s.get(k[0])
 
+    def global_get(self, gk):
+        return self.get([":".join(gk)])
+
     def set(self, k, v):
         self.s[k[0]] = v
+
+    def global_set(self, gk, v):
+        return self.set([":".join(gk)], v)
 
 
 def mockplugin():
@@ -113,12 +119,27 @@ class TestStartup(unittest.TestCase):
     def testFileshare(self):
         p = mockplugin()
         fs = MagicMock()
-        p.get_local_ip = MagicMock(return_value="111.111.111.111")
+        p.get_local_addr = lambda: ("111.111.111.111:0")
         p._file_manager.path_on_disk.return_value = "/testpath"
 
         p._init_fileshare(fs_cls=fs)
 
         fs.assert_called_with("111.111.111.111:0", "/testpath", logging.getLogger())
+
+    def testFileshareAddrFailure(self):
+        p = mockplugin()
+        fs = MagicMock()
+        p.get_local_addr = MagicMock(side_effect=[OSError("testing")])
+        p._init_fileshare(fs_cls=fs)  # Does not raise exception
+        self.assertEqual(p._fileshare, None)
+
+    def testFileshareConnectFailure(self):
+        p = mockplugin()
+        fs = MagicMock()
+        p.get_local_addr = lambda: "111.111.111.111:0"
+        fs.connect.side_effect = OSError("testing")
+        p._init_fileshare(fs_cls=fs)  # Does not raise exception
+        self.assertEqual(p._fileshare, fs())
 
     def testQueues(self):
         p = mockplugin()
@@ -551,3 +572,25 @@ class TestCleanupFileshare(unittest.TestCase):
             self.assertTrue((p / f"{n}.gcode").exists())
         self.assertFalse((p / "c.gcode").exists())
         self.assertFalse((p / "d").exists())
+
+
+class TestLocalAddressResolution(unittest.TestCase):
+    def setUp(self):
+        self.p = mockplugin()
+
+    @patch("continuousprint.plugin.socket")
+    def testResolutionViaCheckAddrOK(self, msock):
+        self.p._settings.global_set(["server", "onlineCheck", "host"], "checkhost")
+        self.p._settings.global_set(["server", "onlineCheck", "port"], 5678)
+        s = msock.socket()
+        s.getsockname.return_value = ("1.2.3.4", "1234")
+        self.assertEqual(self.p.get_local_addr(), "1.2.3.4:1234")
+        s.connect.assert_called_with(("checkhost", 5678))
+
+    @patch("continuousprint.plugin.socket")
+    def testResolutionFailoverToMDNS(self, msock):
+        self.p._can_bind_addr = lambda a: False
+        msock.gethostbyname.return_value = "1.2.3.4"
+        s = msock.socket()
+        s.getsockname.return_value = ("ignored", "1234")
+        self.assertEqual(self.p.get_local_addr(), "1.2.3.4:1234")
